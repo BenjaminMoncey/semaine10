f90f048c1b3fb6d353976f6222052601f408d383
benjaminmoncey <benjamin.moncey@gmail.com>
2016-01-12 13:52:23 +0000

    js ok

--- cookieclicker/js/cookie.js
+++ cookieclicker/js/cookie.js
@@ -6,21 +6,24 @@ var compteur = 1;
 var prix = function () {
     return 50 * compteur;
 };
-
-bouton.addEventListener('click', function () {
-    score += 1;
-    score = score + compteur - 1;
-    if (score === 1) {
+var afficherScore = function () {
+    if (score < 2) {
         affichage.innerHTML = score + " cookie";
     } else {
         affichage.innerHTML = score + " cookies";
     }
+};
+
+bouton.addEventListener('click', function () {
+    score += 1;
+    score = score + compteur - 1;
+    afficherScore();
 }, false);
 
 var autoclick = function () {
     if (score >= 200) {
         score += 1;
-        affichage.innerHTML = score + " cookies";
+        afficherScore();
     }
 };
 
@@ -30,7 +33,7 @@ var incrementer = function () {
     if (score >= prix()) {
         score -= prix();
         compteur += 1;
-        affichage.innerHTML = score + " cookies";
+        afficherScore();
         multiplicateur.innerHTML = "Multiplicateur x" + compteur + " - prochain prix = " + prix();
     } else {
         alert("Pas assez de cookies !");
